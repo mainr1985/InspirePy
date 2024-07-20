9724aab7166d6c1082ba0254cd9ea304431d5282
Maíra N. Rocha <mairanrocha@hotmail.com>
2024-07-20 13:48:35 -0300

    Criação do projeto InspirePy

---
+++ inspirePy.py
@@ -0,0 +1,5 @@
+import random
+from quotes import quotes
+
+chosen_quote=random.choice(quotes)
+print(chosen_quote)
---
+++ quotes.py
@@ -0,0 +1,13 @@
+quotes = [
+    "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. - Dr. Seuss",
+    "To bring about change, you must not be afraid to take the first step.\n We will fail when we fail to try. — Rosa Parks",
+    "It is during our darkest moments that we must focus to see the light. - Aristotle",
+    "Got has provided - The Bible",
+    "Nothing is impossible. The word itself says 'I'm possible! - Audrey Hepburn",
+    "All our dreams can come true, if we have the courage to pursue them. - Walt Disney",
+    "Don't sit down and wait for the opportunities to come. Get up and make them. - Madam C.J. Walker",
+    "Your playing small does not serve the world. - Marianne Williamson",
+    "As we let our own light shine, we unconsciously give other people permission to do the same. As we are liberated from our own fear, our presence automatically liberates others. - Marianne Williamson",
+    "There's nothing to fear, but fear itself. Push on! - Franklin D. Roosevelt",
+    "Yes, you are good enough! Just keep trying. You'll get there."
+]
